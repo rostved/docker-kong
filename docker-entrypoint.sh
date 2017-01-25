9f855ed98b2deeb86fbb6c2e7fd6c343663823c5
Mikkel Rostved <miro@energidata.dk>
2017-01-25 09:47:43 +0100

    Set Kong cluster_listen to the local containers primary IP using Rancher metadata

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,8 @@ export KONG_NGINX_DAEMON="off"
 
 [ -z "$KONG_NGINX_DAEMON" ] && export KONG_NGINX_DAEMON="off"
 
+# Set cluster listen address using IP from Rancher metadata
+export KONG_CLUSTER_LISTEN="$(curl --retry 3 --fail --silent http://rancher-metadata/2015-07-25/self/container/primary_ip):7946"
+
 exec "$@"
+
